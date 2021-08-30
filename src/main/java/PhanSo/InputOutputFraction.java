6a050b1626539f524f76213c8e3936b06a0f8a10
MaiNguyenSuri <snow_white_1501@hotmail.com>
2021-08-30 08:58:44 +0700

    Thêm bài tính tổng phân số

--- src/main/java/PhanSo/InputOutputFraction.java
+++ src/main/java/PhanSo/InputOutputFraction.java
@@ -9,7 +9,7 @@ public class InputOutputFraction {
 
     public InputOutputFraction(float numerator, float denominator) {
         this.numerator = numerator;
-        if(denominator != 0) {
+        if (denominator != 0) {
             this.denominator = denominator;
         }
     }
@@ -17,13 +17,39 @@ public class InputOutputFraction {
     public InputOutputFraction() {
     }
 
-    public float fraction(){
+    public static InputOutputFraction sumFraction(InputOutputFraction frt1, InputOutputFraction frt2) {
+        InputOutputFraction frt3 = new InputOutputFraction();
+
+        float tuso;
+        float mauso;
+        if (frt1.getDenominator() == frt2.getDenominator()) {
+            tuso = frt1.getNumerator() + frt2.getNumerator();
+            mauso = frt1.getDenominator();
+
+
+        } else {
+            tuso = (frt1.getNumerator() * frt2.getDenominator()) + (frt2.getNumerator() * frt1.getDenominator());
+            mauso = frt1.getDenominator() * frt2.getDenominator();
+        }
+
+        frt3.setNumerator(tuso);
+        frt3.setDenominator(mauso);
+
+        return frt3.compact();
+    }
+
+    public float fraction() {
         return numerator / denominator;
     }
 
-    public String fractionExport(){
+    public String fractionExport() {
         String export = String.format("%.0f/%.0f", numerator, denominator);
         return export;
+    }
+
+    public void print() {
+        String export = fractionExport();
+        System.out.println(export);
     }
 
     public float getDenominator() {
@@ -31,7 +57,7 @@ public class InputOutputFraction {
     }
 
     public void setDenominator(float denominator) {
-        if(denominator != 0) {
+        if (denominator != 0) {
             this.denominator = denominator;
         }
     }
@@ -43,4 +69,16 @@ public class InputOutputFraction {
     public void setNumerator(float numerator) {
         this.numerator = numerator;
     }
+
+    public InputOutputFraction compact() {
+        CompactFraction compact = new CompactFraction(this);
+        return compact.getFraction();
+    }
+
+    public InputOutputFraction sum(InputOutputFraction frtinput) {
+        InputOutputFraction frt3 = sumFraction(this, frtinput);
+
+        return frt3;
+    }
+
 }
--- src/main/java/PhanSo/Run.java
+++ src/main/java/PhanSo/Run.java
@@ -7,18 +7,20 @@ public class Run {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //PHÂN SỐ 1
+        System.out.println("Phân số 1: ");
         System.out.println("Nhập tử số: ");
-        float tuSo = scanner.nextFloat();
+        float tuSo1 = scanner.nextFloat();
 
         System.out.println("Nhập mẫu số: ");
-        float mauSo = scanner.nextFloat();
+        float mauSo1 = scanner.nextFloat();
 
-        InputOutputFraction frt = new InputOutputFraction(tuSo, mauSo);
-        String export = frt.fractionExport();
-        System.out.println("Tử số / Mẫu số là: " + export);
+        InputOutputFraction frt1 = new InputOutputFraction(tuSo1, mauSo1);
+        System.out.print("Tử số / Mẫu số là: ");
+        frt1.print();
 
-        float result = frt.fraction();
-        System.out.println("Kết quả là: " + result);
+        float result1 = frt1.fraction();
+        System.out.println("Kết quả là: " + result1);
 
 
         /*InputOutputFraction frt2 = new InputOutputFraction();
@@ -27,11 +29,49 @@ public class Run {
         float result1 = frt2.fraction();
         System.out.println("Kết quả rút gọn là: " + result1);*/
 
-        CompactFraction cpt = new CompactFraction(frt);
-        cpt.compact();
-        InputOutputFraction frtCpt = cpt.getFraction();
-        System.out.format("Kết quả rút gọn là: %.0f/%.0f", frtCpt.getNumerator(), frtCpt.getDenominator());
+        CompactFraction cpt1 = new CompactFraction(frt1);
+        cpt1.compact();
+        InputOutputFraction frtCpt1 = cpt1.getFraction();
+        System.out.format("Kết quả rút gọn của phân số 1 là: %.0f/%.0f", frtCpt1.getNumerator(), frtCpt1.getDenominator());
         System.out.println();
 
+        ///PHÂN SỐ 2
+
+        System.out.println("Phân số 2: ");
+        System.out.println("Nhập tử số: ");
+        float tuSo2 = scanner.nextFloat();
+
+        System.out.println("Nhập mẫu số: ");
+        float mauSo2 = scanner.nextFloat();
+
+        InputOutputFraction frt2 = new InputOutputFraction(tuSo2, mauSo2);
+        System.out.print("Tử số / Mẫu số là: ");
+        frt2.print();
+
+        float result2 = frt2.fraction();
+        System.out.println("Kết quả là: " + result2);
+
+
+        /*InputOutputFraction frt2 = new InputOutputFraction();
+        frt2.setNumerator(tuSo);
+        frt2.setDenominator(mauSo);
+        float result1 = frt2.fraction();
+        System.out.println("Kết quả rút gọn là: " + result1);*/
+
+        CompactFraction cpt2 = new CompactFraction(frt2);
+        cpt2.compact();
+        InputOutputFraction frtCpt2 = cpt2.getFraction();
+        System.out.print("Kết quả rút gọn là: ");
+        frtCpt2.print();
+        System.out.println();
+
+        //InputOutputFraction sum = InputOutputFraction.sumFraction(frt1,frt2); // có static
+        InputOutputFraction sum = frt2.sum(frt1); //ko static
+        System.out.print("SUM: ");
+        sum.print();
+
+
     }
+
+
 }
